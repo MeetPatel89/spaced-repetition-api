9fb1bc99c6803a3a017d16d548585c27cc155f5f
Meet Patel <meetaiims@yahoo.com>
2021-01-21 23:28:02 -0600

    add POST /guess route to language router

--- src/language/language-router.js
+++ src/language/language-router.js
@@ -65,6 +65,60 @@ languageRouter.get('/head', async (req, res, next) => {
   }
 });
 
+languageRouter.route('/guess').post(bodyParser, async (req, res, next) => {
+  if (!Object.keys(req.body).includes('guess')) {
+    return res.status(400).json({
+      error: `Missing 'guess' in request body`,
+    });
+  }
 
+  const words = await LanguageService.getLanguageWords(
+    req.app.get('db'),
+    req.language.id
+  );
+
+  const wordList = LanguageService.fillList(
+    req.app.get('db'),
+    req.language,
+    words
+  );
+
+  if (req.body.guess === wordList.head.value.translation) {
+    wordList.head.value.correct_count++;
+    wordList.head.value.memory_value =
+      wordList.head.value.memory_value * 2 >= wordList.listNodes().length
+        ? wordList.listNodes().length - 1
+        : wordList.head.value.memory_value * 2;
+    wordList.total_score++;
+    wordList.moveHeadBy(wordList.head.value.memory_value);
+    LanguageService.persistLinkedList(req.app.get('db'), wordList).then(() => {
+      res.json({
+        nextWord: wordList.head.value.original,
+        wordCorrectCount: wordList.head.value.correct_count,
+        wordIncorrectCount: wordList.head.value.incorrect_count,
+        totalScore: wordList.total_score,
+        answer: req.body.guess,
+        isCorrect: true,
+      });
+      next();
+    });
+  } else {
+    wordList.head.value.incorrect_count++;
+    wordList.head.value.memory_value = 1;
+    let rightAnswer = wordList.head.value.translation;
+    wordList.moveHeadBy(wordList.head.value.memory_value);
+    LanguageService.persistLinkedList(req.app.get('db'), wordList).then(() => {
+      res.json({
+        nextWord: wordList.head.value.original,
+        wordCorrectCount: wordList.head.value.correct_count,
+        wordIncorrectCount: wordList.head.value.incorrect_count,
+        totalScore: wordList.total_score,
+        answer: rightAnswer,
+        isCorrect: false,
+      });
+      next();
+    });
+  }
+});
 
 module.exports = languageRouter;

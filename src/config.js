b38c0ab01532a0c4184d9cfa7e466b16565ae967
Meet Patel <meetaiims@yahoo.com>
2021-01-26 10:42:29 -0600

    refactor config

--- src/config.js
+++ src/config.js
@@ -2,8 +2,9 @@ module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
   DATABASE_URL:
-    process.env.DATABASE_URL ||
-    'postgresql://dunder-mifflin@localhost/spaced-repetition',
+    process.env.DATABASE_URL +
+      (process.env.NODE_ENV === 'production' ? '?ssl=true' : '') ||
+    'postgresql://nautilus_shell:Fibonaccispiral89@localhost/spaced-repetition',
   TEST_DATABASE_URL: process.env.TEST_DATABASE_URL,
   JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret',
   JWT_EXPIRY: process.env.JWT_EXPIRY || '3h',

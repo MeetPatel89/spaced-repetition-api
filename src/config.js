47c02685c3fdec6b157c905098679f39d3df6411
Meet Patel <meetaiims@yahoo.com>
2021-01-26 12:29:27 -0600

    refactor routes

--- src/config.js
+++ src/config.js
@@ -5,6 +5,11 @@ module.exports = {
     process.env.DATABASE_URL ||
     'postgresql://nautilus_shell:Fibonaccispiral89@localhost/spaced-repetition',
   TEST_DATABASE_URL: process.env.TEST_DATABASE_URL,
+  JWT_SECRET: '$2a$10$fCWkaGbt7ZErxaxclioLteLUgg4Q3Rp09WW0s/wSLxDKYsaGYUpjG',
+  JWT_EXPIRY: '3h'
+  /*
   JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret',
+  /*
   JWT_EXPIRY: process.env.JWT_EXPIRY || '3h',
+  */
 };
